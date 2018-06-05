5cea87b73a5bf8ef32e23a37f9dcd0aa0342cbdc
metry <fortrun.php@gmail.com>
2018-06-05 11:18:05 +0300

    #0001 add homework N2

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+require('src/functions.php');
+
+task1(['1 piece','2 piece','3 piece']);
+echo task1(['1 piece','2 piece','3 piece'], true);
+
+echo '<br>';
+echo task2('/', 12, 1, 1, 1.2);
+echo '<br>';
+echo task2('+', 12);
+echo '<br>';
+echo task2('#', 12, 1, 1, 1.2);
+echo task2('+');
+
+echo '<br>';
+task3(8,8);
+echo '<br>';
+task3("a",8);
+echo '<br>';
+task3(3,5);
+echo '<br>';
+task3(-2,0);
---
+++ src/functions.php
@@ -0,0 +1,75 @@
+<?php
+
+function task1(array $arr, bool $hasReturned = false)
+{
+    if ($hasReturned) {
+        return implode('', $arr);
+    } else {
+        if (!empty($arr)) {
+            foreach ($arr as $item) {
+                echo '<p>' . $item . '</p>';
+            }
+        }
+        return null;
+    }
+}
+
+function task2(string $str, ...$numbers)
+{
+    if ($numbers) {
+        $result = 0;
+        switch ($str) {
+            case '+':
+                foreach ($numbers as $number) {
+                    $result += $number;
+                }
+                break;
+            case '-':
+                foreach ($numbers as $numberKey => $numberVal) {
+                    if ($numberKey == 0) {
+                        $result = $numberVal;
+                    } else {
+                        $result -= $numberVal;
+                    }
+                }
+                break;
+            case '*':
+                $result = 1;
+                foreach ($numbers as $number) {
+                    $result *= $number;
+                }
+                break;
+            case '/':
+                foreach ($numbers as $numberKey => $numberVal) {
+                    if ($numberKey == 0) {
+                        $result = $numberVal;
+                    } else {
+                        $result /= $numberVal;
+                    }
+                }
+                break;
+            default:
+                return null;
+        }
+        echo implode(' ' . $str . ' ', $numbers) . ' = ' . $result;
+    } else {
+        return null;
+    }
+}
+
+function task3($a, $b)
+{
+    if (is_int($a) && is_int($b) && $a > 0 && $b > 0) {
+        echo '<table border="1">';
+        for ($i = 1; $i <= $a; $i++) {
+            echo '<tr>';
+            for ($j = 1; $j <= $b; $j++) {
+                echo '<td style="padding:20px;">' . $i*$j . '</td>';
+            }
+            echo '</tr>';
+        }
+        echo '</table>';
+    } else {
+        echo 'Аргументы должны представлять собой целые положительные числа';
+    }
+}
