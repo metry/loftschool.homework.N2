5fac5497294c81540b527250277028fc8677cacb
metry <fortrun.php@gmail.com>
2018-06-07 13:04:53 +0300

    #0001 psr-2 format code fix

--- index.php
+++ index.php
@@ -13,10 +13,10 @@ echo '<br>';
 echo task2('#', 12, 1, 1, 1.2);
 
 echo '<br>';
-task3(8,8);
+task3(8, 8);
 echo '<br>';
-task3("a",8);
+task3("a", 8);
 echo '<br>';
-task3(3,5);
+task3(3, 5);
 echo '<br>';
-task3(-2,0);
+task3(-2, 0);
